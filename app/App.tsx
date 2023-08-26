57550db2e6965b947f0c74f819577685277016f9
xiaominliu <xiaomin@vantage.sh>
2023-08-26 17:45:42 -0400

    feat: replace template app with chessboard

--- app/App.tsx
+++ app/App.tsx
@@ -1,31 +1,40 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import "./App.css";
+import React, { useState } from "react";
+import { Chessboard } from "react-chessboard";
+import { Chess, Move, Square } from "chess.js";
 
-function App() {
-  const [count, setCount] = useState(0);
+const App: React.FC = () => {
+  const [game, setGame] = useState(new Chess());
+
+  const handleMove = (sourceSquare: string, targetSquare: string): boolean => {
+    let moveSuccessful = false;
+
+    try {
+      // TODO: Handle promotion
+      const moveResult: Move = game.move({
+        from: sourceSquare,
+        to: targetSquare,
+      });
+
+      if (moveResult) {
+        setGame(new Chess(game.fen()));
+        moveSuccessful = true;
+      }
+    } catch (error) {
+      console.error("Illegal move attempted: ", error);
+    }
+
+    return moveSuccessful;
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <div className="app">
+      <Chessboard
+        position={game.fen()}
+        boardWidth={500} // TODO: Make this responsive
+        onPieceDrop={(sourceSquare: Square, targetSquare: Square) => handleMove(sourceSquare, targetSquare)}
+      />
+    </div>
   );
-}
+};
 
 export default App;

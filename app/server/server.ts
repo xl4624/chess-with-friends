9d467c573d7727a4c9fd61911b8c5e95d1372d3c
Xiaomin Liu Liu <116298054+xl4624@users.noreply.github.com>
2023-09-03 00:20:31 -0400

    feat: setup database migrations

--- app/server/server.ts
+++ app/server/server.ts
@@ -1,8 +1,7 @@
 import express from "express";
-import dotenv from "dotenv";
-import { DataSource } from "typeorm";
 import "reflect-metadata";
-import { User } from "../../db/entity/User.ts";
+
+import { AppDataSource } from "../../db/data-source.ts";
 
 const app = express();
 const PORT = 3001;
@@ -11,26 +10,15 @@ app.get("/api", (_req, res) => {
   res.send("Server is running");
 });
 
+app.get("/api/users", (_req, res) => {
+  try {
+    const users = AppDataSource.manager.find("User");
+    res.json(users);
+  } catch (err: unknown) {
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-dotenv.config();
-
-export const AppDataSource = new DataSource({
-  type: "postgres",
-  host: process.env.DB_HOST!,
-  port: Number(process.env.DB_PORT!),
-  database: process.env.DB_NAME!,
-  username: process.env.DB_USER!,
-  password: process.env.DB_PASSWORD!,
-  entities: [User],
-});
-
-AppDataSource.initialize()
-  .then(() => {
-    console.log("Data Source has been initialized!");
-  })
-  .catch((err: unknown) => {
-    console.error("Error during Data Source initialization", err);
-  });

6f2949dabb65e1bbd7e71855d5358a52219a5d21
kcao20 <kcao2022@gmail.com>
2023-09-09 19:03:34 -0400

    fix: add removed rating column

--- db/migrations/1693853371298-UpdateUserEntity.ts
+++ db/migrations/1693853371298-UpdateUserEntity.ts
@@ -4,6 +4,7 @@ export class UpdateUserEntity1693853371298 implements MigrationInterface {
   name = "UpdateUserEntity1693853371298";
 
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" ADD "rating" integer NOT NULL DEFAULT '1000'`);
     await queryRunner.query(`ALTER TABLE "user" ADD "password" character varying(64) NOT NULL`);
     await queryRunner.query(`ALTER TABLE "user" ADD "email" text NOT NULL`);
     await queryRunner.query(`ALTER TABLE "user" ADD CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email")`);
@@ -13,5 +14,6 @@ export class UpdateUserEntity1693853371298 implements MigrationInterface {
     await queryRunner.query(`ALTER TABLE "user" DROP CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22"`);
     await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "email"`);
     await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "password"`);
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "rating"`);
   }
 }

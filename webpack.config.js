e5ea8fc9de54f6830b75f7c898423c3683f39913
Xiaomin Liu <xl4624@nyu.edu>
2024-01-03 19:11:07 -0500

    Make webpack search for JavaScript files to add
    Might have to remove the dist folder from `static/js`, since now the
    dist folder will be outside of `static/js`.

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { glob } from 'glob';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default {
-    entry: {
-        game_view: './static/js/game_view.js',
-        waiting_room: './static/js/waiting_room.js',
-    },
+    entry: glob.sync('./static/js/**/*.js').reduce((acc, path) => {
+        const filePath = path.startsWith('./') ? path : './' + path;
+        const entry = filePath
+            .replace('./static/js/', '')
+            .replace(/\.js$/, '');
+        acc[entry] = filePath;
+        return acc;
+    }, {}),
     output: {
-        path: path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'static/js/dist'),
+        path: path.resolve(__dirname, 'static/dist'),
         filename: '[name].bundle.js', 
     },
 };

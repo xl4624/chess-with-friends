078e8cf18dd72308583392493befa2ff94fc6856
Xiaomin Liu <xl4624@nyu.edu>
2024-01-08 00:09:49 -0500

    Move JavaScript from index.html tags to index.js

---
+++ static/js/index.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("createGameButton").addEventListener("click", function() {
+        fetch("games/create/", {
+            method: "POST",
+        })
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            if (data.redirect) {
+                window.location.href = data.redirect;
+            }
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    });
+});
+
